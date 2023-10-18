b6f84a20d726345f233e9646b894dba9946456db
Greatemmet <greatemmet@gmail.com>
2023-10-18 15:38:41 +0100

    Made implementations on App.js - Created state to manage darkmode property and Navbar.js - Create a toggle button which will toggle the darkmode state

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,16 @@ import Navbar from "./components/Navbar/Navbar";
 import Main from "./components/Main/Main";
 
 function App() {
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  function toggle() {
+    setDarkMode((prev) => !prev);
+  }
+
   return (
     <div className="container">
-      <Navbar/>
-      <Main/>
+      <Navbar darkMode={darkMode} toggledDarkMode={toggle} />
+      <Main darkMode={darkMode} />
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,20 @@
 import "./Navbar.scss";
 
 import navLogo from "../../assets/png/react-blue.png";
-export default function Navbar() {
+export default function Navbar(props) {
   return (
-    <div className="navbar">
+    <div className={`navbar ${props.darkMode ? "dark" : ""}`}>
       <img src={navLogo} alt="" className="navLogo" />
       <h1 className="navTitle">ReactFacts</h1>
-      <h3 className="navProject">React Course - Project 1</h3>
+      <div className="navProject">
+        <div className="navToggler">
+          <p>Light</p>
+          <div className="navToggleSlider">
+            <div className="navToggleSliderCircle"></div>
+          </div>
+          <p>Dark</p>
+        </div>
+      </div>
     </div>
   );
 }
